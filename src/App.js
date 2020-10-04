004abefad4c964a55c1ea1aea35503242d418b93
elenaBushueva <helenabushueva@gmail.com>
2020-10-03 23:58:10 -0700

    todo_2lists

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const initState1 = [
+        {id: 1, name: 'Vera'},
+        {id: 2, name: 'Nico'},
+    ];
+
+    const initState2 = [
+        {id: 3, name: 'Kate'},
+        {id: 4, name: 'John'},
+    ];
+
+    const [list1, setList1] = useState(initState1);
+    const [list2, setList2] = useState(initState2);
+
+    const moveToList2 = (id) => {
+        const moveItem = list1.find(el => (el.id === id));
+        setList2([...list2, moveItem]);
+        setList1(list1.filter(el => el.id !== id));
+    }
+
+    const moveToList1 = (id) => {
+        const moveItem = list2.find(el => (el.id === id));
+        setList1([...list1, moveItem]);
+        setList2(list2.filter(el => el.id !== id));
+    }
+
+    return (
+        <ol>
+            <h4>List 1</h4>
+            {list1.map(el =>
+                <li>
+                    {el.name}
+                    <button onClick={() => moveToList2(el.id)}>move to list 2</button>
+                </li>)}
+            <hr/>
+            <h4>List 2</h4>
+            {list2.map(el =>
+                <li>
+                    {el.name}
+                    <button onClick={() => moveToList1(el.id)}>move to list 1</button>
+                </li>)}
+                <hr/>
+        </ol>
+    );
 }
 
 export default App;
